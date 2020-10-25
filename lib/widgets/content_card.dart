798262311a775b40d0e5014889906f27540699ff
Matija Osojnik <matija1.osojnik@gmail.com>
2020-10-25 11:58:01 +0100

    Start Firebase Before Flutter App

--- lib/widgets/content_card.dart
+++ lib/widgets/content_card.dart
@@ -1,27 +1,27 @@
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
 
-class ContentCard extends StatelessWidget {
-  ContentCard({this.imageUrl, this.title, this.description});
+// class ContentCard extends StatelessWidget {
+//   ContentCard({this.imageUrl, this.title, this.description});
 
-  final imageUrl, title, description;
+//   final imageUrl, title, description;
 
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return Center(
-      child: Card(
-        child: InkWell(
-          splashColor: Colors.blue.withAlpha(30),
-          onTap: () {
-            print('Card tapped.');
-          },
-          child: Container(
-            width: 300,
-            height: 100,
-            child: Text('A card that can be tapped'),
-          ),
-        ),
-      ),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     // TODO: implement build
+//     return Center(
+//       child: Card(
+//         child: InkWell(
+//           splashColor: Colors.blue.withAlpha(30),
+//           onTap: () {
+//             print('Card tapped.');
+//           },
+//           child: Container(
+//             width: 300,
+//             height: 100,
+//             child: Text('A card that can be tapped'),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
