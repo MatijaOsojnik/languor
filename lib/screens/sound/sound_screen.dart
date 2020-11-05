947aabc215e9b0d8efd5e7608c7c5124d1256839
Matija Osojnik <matija1.osojnik@gmail.com>
2020-11-05 20:32:31 +0100

    Create Sound Screen Design, Back Button Functionality

--- lib/screens/sound/sound_screen.dart
+++ lib/screens/sound/sound_screen.dart
@@ -1,7 +1,9 @@
+import 'package:firebase_image/firebase_image.dart';
 import 'package:flutter/material.dart';
 import 'package:Languor/models/sound.dart';
 import 'package:provider/provider.dart';
 import 'package:Languor/services/database.dart';
+import 'package:firebase_image/firebase_image.dart';
 
 class SoundScreen extends StatelessWidget {
   final Sound sound;
@@ -12,22 +14,63 @@ class SoundScreen extends StatelessWidget {
         initialData: const [],
         value: DatabaseService().sounds,
         child: MaterialApp(
-          home: Scaffold(
-            appBar: AppBar(
-              actions: [
-                IconButton(
-                  key: const ValueKey("goBack"),
-                  icon: const Icon(Icons.exit_to_app),
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                )
-              ],
-            ),
-            body: Container(
-              child: Text(sound.name),
-            ),
-          ),
-        ));
+            home: Scaffold(
+                backgroundColor: Colors.white,
+                appBar: AppBar(
+                  leading: IconButton(
+                    key: const ValueKey("goBack"),
+                    icon: const Icon(Icons.cancel_sharp),
+                    iconSize: 30.0,
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                  ),
+                ),
+                body: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Column(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(10.0),
+                              child: Text(
+                                sound.name,
+                                style: const TextStyle(fontSize: 30),
+                              ),
+                            ),
+                            Text(
+                              sound.description,
+                              style: const TextStyle(fontSize: 18),
+                            ),
+                          ],
+                        ),
+                        Image(
+                          image: FirebaseImage(sound.imageUrl),
+                          height: 300,
+                          width: 300,
+                        ),
+                        SizedBox(
+                            width: MediaQuery.of(context).size.width,
+                            height: 70,
+                            child: Padding(
+                              padding: const EdgeInsets.only(
+                                  left: 20.0, right: 20.0),
+                              child: RaisedButton(
+                                textColor: Colors.white,
+                                color: Colors.blue,
+                                onPressed: () => {Navigator.pop(context)},
+                                child: const Text(
+                                  'Begin',
+                                  style: TextStyle(fontSize: 20.0),
+                                ),
+                              ),
+                            ))
+                      ],
+                    ),
+                  ],
+                ))));
   }
 }
